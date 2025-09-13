da2062ead811439f092cbf5d80347d686c88ee8e
hananmjamal <maryam.hanan02@gmail.com>
2025-09-13 19:45:01 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,22 @@
+x=("hello")
+y=("world")
+z=x+y
+print(z)
+
+n1=input("Enter 1st number: ")
+n2=input("Enter 2nd number: ")
+
+b=int(n1)+int(n2)
+print("Addition: ",b)
+
+c=int(n1)-int(n2)
+print("Subtraction: ",c)
+
+d=int(n1)*int(n2)
+print("Multiplication: ",d)
+
+e=int(n1)/int(n2)
+print("Division: ",e)
+
+a=n1+n2
+print("Concatenation: ",a)
